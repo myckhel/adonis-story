f1b43804bc9003792b193a5a30b8f3baaa515602
myckhel <myckhel1@hotmail.com>
2021-08-16 21:45:14 +0100

    story migration

---
+++ database/migrations/1629143013556_stories.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Stories extends BaseSchema {
+  protected tableName = 'stories'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
+      table.string('color', 7).nullable()
+      table.text('text').nullable()
+      table.string('font', 100).nullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
